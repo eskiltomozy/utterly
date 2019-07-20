36159af859d17c5e7e5f5017494ebf12dff4cb33
dowuh <eskil@tomozy.com>
2019-07-20 15:58:55 +0000

    TDD 3:7 - gram to database creation

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
   describe 'grams#index action' do
-    it "should successfully show the page" do
+    it 'should successfully show the page' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'grams#new action' do
+    it 'should successfully show new form' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'grams#create action' do
+    it 'should successfully cretae new gram within database' do
+      post :create, params: {gram: {message: 'Hello'}}
+      expect(response).to redirect_to root_path
+      gram = Gram.last
+      expect(gram.message).to eq('Hello')
     end
   end
 end
